05eac9ca0b391cee99ac1849575fffff876c681f
Pack <packaq3@gmail.com>
2022-08-15 22:27:47 +0700

    Initial Commit

---
+++ EP_3 _I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EP_3 _I Am Poor
+//
+//  Created by Pipat on 15/8/2565 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
